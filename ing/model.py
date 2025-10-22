9de2e948b97631cfd81c6e15246f3c945e81d550
Lars Wagner <wagner1975@web.de>
2025-10-22 22:44:24 +0200

    first enum and model classes

--- ing/model.py
+++ ing/model.py
@@ -1,15 +1,31 @@
+from enum import Enum
 from dataclasses import dataclass
+from datetime import datetime
+from decimal import Decimal
+
+
+class TransactionTypeEnum(str, Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+class CurrencyEnum(str, Enum):
+    EUR = "EUR"
+    USD = "USD"
+
+
+class Money:
+    amount: Decimal
+    currency: CurrencyEnum
 
 
 @dataclass
-class SecuritiesSettlementModel:
-    kauf_verkauf: str
+class SecuritiesSettlement:
+    transaction_type: TransactionTypeEnum
+    execution_time: datetime
     isin: str
-    wkn: str
-    nominale: str
-    kurs: str
-    kurswert: str
-    ausfuehrzeit: str
-    provision: str
-    endbetrag_lasten: str
-    endbetrag_gunsten: str
+    shares: Decimal # Nominale
+    price_per_share: Decimal # Kurs
+    market_value: str # Kurswert
+    provision: Money
+    final_amount: Money # negative: to be charged / positive: in favor of
